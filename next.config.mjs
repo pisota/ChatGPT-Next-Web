48bf1b6af3935549b33e61abc58bdf605c05eb76
pisota <pisota@gmail.com>
2023-12-28 15:03:25 +0800

    更改中转地址
    更改中转地址到oneapi.xty.app

--- next.config.mjs
+++ next.config.mjs
@@ -66,7 +66,7 @@ if (mode !== "export") {
     const ret = [
       {
         source: "/api/proxy/:path*",
-        destination: "https://api.openai.com/:path*",
+        destination: "https://oneapi.xty.app/:path*",
       },
       {
         source: "/google-fonts/:path*",
